b0559281ac1e506bd5e9667f6b30a094bd62403b
Minata P Mahaarta <minataputra@gmail.com>
2024-10-15 11:32:47 +0700

    Initial Commit

---
+++ SwiftUI Warmingup/SwiftUI_WarmingupApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WarmingupApp.swift
+//  SwiftUI Warmingup
+//
+//  Created by Minata on 15/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WarmingupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
